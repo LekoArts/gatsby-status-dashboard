6907e7401311b43b7a5cc7b1264c63763d270a3c
LekoArts <lekoarts@gmail.com>
2019-07-24 12:11:43 +0200

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({
+require(`dotenv`).config({
   path: `.env`,
 })
 
@@ -6,39 +6,26 @@ module.exports = {
   siteMetadata: {
     siteUrl: process.env.SITE_URL,
     siteName: process.env.SITE_TITLE,
-    siteDescription: process.env.SITE_DESCRIPTION
+    siteDescription: process.env.SITE_DESCRIPTION,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-typescript',
+    `@lekoarts/gatsby-theme-status-dashboard`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
     {
-      resolve: 'gatsby-source-netlify',
-      options: {
-        apiKey: process.env.NETLIFY_ACCESS_KEY,
-      }
-    },
-    {
-      resolve: 'gatsby-source-circleci',
-      options: {
-        apiKey: process.env.CIRCLECI_KEY,
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
         name: process.env.SITE_URL,
-        short_name: 'Status',
+        short_name: `Status`,
         description: process.env.SITE_DESCRIPTION,
-        start_url: '/',
-        background_color: '#002845',
-        theme_color: '#fff9d4',
-        display: 'standalone',
-        icon: 'src/favicon.png',
+        start_url: `/`,
+        background_color: `#f0f2fd`,
+        theme_color: `#3490dc`,
+        display: `standalone`,
+        icon: `assets/favicon.png`,
       },
     },
-    /* Must be placed at the end */
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ]
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
 }
--- src/typings.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'tailwind.macro'
-declare module '*.svg'
